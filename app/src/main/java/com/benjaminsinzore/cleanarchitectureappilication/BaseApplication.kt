e368eb23e432a9b078cda7d8464ba89a8480ece4
benjiesinzore <benjaminsinzore@gmail.com>
2022-11-07 23:33:34 +0300

    Initial Commit on Dependencies.

--- app/src/main/java/com/benjaminsinzore/cleanarchitectureappilication/BaseApplication.kt
+++ app/src/main/java/com/benjaminsinzore/cleanarchitectureappilication/BaseApplication.kt
@@ -3,6 +3,6 @@ package com.benjaminsinzore.cleanarchitectureappilication
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
-class BaseApplication : Application() {
+
+class BaseApplication {
 }
