1dc536df8ad2562c72a0d993e0b289256aecb0a2
benjiesinzore <benjaminsinzore@gmail.com>
2022-11-23 22:09:35 +0300

    Compose UI 1. HomeScreen

--- data/src/main/java/com/benjaminsinzore/data/network/ApiService.kt
+++ data/src/main/java/com/benjaminsinzore/data/network/ApiService.kt
@@ -1,9 +1,20 @@
 package com.benjaminsinzore.data.network
 
+import com.benjaminsinzore.comons.Constance
 import com.benjaminsinzore.data.network.model.BlogsDTO
 import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
 
 interface ApiService {
 
-    suspend fun getBlogs() : Response<BlogsDTO>
+//    suspend fun getBlogs() : Response<BlogsDTO>
+
+
+
+
+    @GET("post")
+    suspend fun getBlogs(
+        @Header("app-id") appId:String= Constance.APP_ID
+    ) : Response<BlogsDTO>
 }
